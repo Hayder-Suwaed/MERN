89c5499ea6a03edf514d8e5e391eab8cf80d7466
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-10 17:01:11 -0600

    added user routes

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express();
 
+// connect Database
+
+connectDB();
+
+// Init Middleware
+app.use(express.json({extened: false }))
+app.get('/', (req, res) => res.send('API Running'))
+
 const PORT = process.env.PORT || 1234
 
-app.get('/', (req, res) => res.send('API Running'))
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
