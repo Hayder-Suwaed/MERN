2271657257eef866350be40d54729ab63cadd48f
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-11 21:13:38 -0600

    get github repo for profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("./middleware/auth");
 const { check, validationResult } = require("express-validator/check");
@@ -330,6 +332,33 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     console.error(err.message);
     res.status(500).send("Server Error");
   }
+});
+
+// @route    DELETE api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" }
+    };
+
+    request(options, (error, responsse, body) => {
+      if (error) console.error(error);
+
+      if (Response.statusCode !== 200) {
+        res.status(404).json({ msg: "No github profile found" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {}
 });
 
 module.exports = router;
