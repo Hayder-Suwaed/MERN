cd8756e204d805868b9c15250005390ec741eddf
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-04-06 22:43:55 -0600

    fix identation

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
-
 const User = require("../../models/User");
+
 // @route  POST api/users
 // @desc   register user
 // @access Public
@@ -14,14 +14,12 @@ const User = require("../../models/User");
 router.post(
   "/",
   [
-    check("name", "Name is required")
-      .not()
-      .isEmpty(),
+    check("name", "Name is required").not().isEmpty(),
     check("email", "Please include a valid email").isEmail(),
     check(
       "password",
       "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 })
+    ).isLength({ min: 6 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -43,14 +41,14 @@ router.post(
       const avatar = gravatar.url(email, {
         s: "200",
         r: "pg",
-        d: "mm"
+        d: "mm",
       });
 
       user = new User({
         name,
         email,
         avatar,
-        password
+        password,
       });
 
       const salt = await bcrypt.genSalt(10);
@@ -61,16 +59,18 @@ router.post(
 
       const payload = {
         user: {
-          id: user.id
-        }
+          id: user.id,
+        },
       };
-      jwt.sign(payload,
+      jwt.sign(
+        payload,
         config.get("jwtSecret"),
-        {expiresIn: 360000},
+        { expiresIn: 360000 },
         (err, token) => {
-          if(err) throw err;
+          if (err) throw err;
           res.json({ token });
-        });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
