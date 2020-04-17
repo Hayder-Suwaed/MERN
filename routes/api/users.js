bbaa722b0a926d5936bdbf76279dad52d3bacbbf
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-16 23:45:10 -0600

    Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
@@ -29,7 +31,9 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-       return  res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -51,9 +55,21 @@ router.post(
 
       await user.save();
 
-      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send("Use registered");
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
