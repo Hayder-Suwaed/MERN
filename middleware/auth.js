4d60bff44a8215c90edc46897cfa4bd0acb6a2bd
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-16 23:58:43 -0600

    added Custom Auth Middleware & JWT Verfiy

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  // Get token from header
-
+  // Get tokeen from header
   const token = req.header("x-auth-token");
 
   // Check if no token
@@ -15,8 +14,9 @@ module.exports = function (req, res, next) {
 
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
-    req.user = decoded.user;
     next();
+
+    req.user = decoded.user;
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
   }
