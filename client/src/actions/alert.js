081767b2ae7dab099a99511ef39700405366ef57
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-18 23:51:45 -0600

    fix identation

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,11 +1,10 @@
-import uuid from 'uuid'
-import { SET_ALERT, REMOVE_ALERT } from './type'
+import uuid from "uuid";
+import { SET_ALERT, REMOVE_ALERT } from "./type";
 
-export const setSlert = (msg, alerType) => dispatch => {
-    const id = uuidv4();
-    dispatch({
-        type: SET_ALERT,
-        payload: {msg, alertType, id}
-    })
-
-}
+export const setSlert = (msg, alerType) => (dispatch) => {
+  const id = uuidv4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+};
