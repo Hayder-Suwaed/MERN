029c3fd2ea4bfe3116b86c1d9a265f83931abe15
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-04-20 05:53:32 -0600

    Edit Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,32 +1,35 @@
-import React, { useEffect, Fragment } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getCurrentProfile } from "../../actions/profile";
-import Spinner from "../layout/Spinner";
-import { Link } from "react-router-dom";
+import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
+import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
   getCurrentProfile,
   auth: { user },
-  profile: { profile, loading },
+  profile: { profile, loading }
 }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
-  return loading & (profile === null) ? (
+  return loading && profile === null ? (
     <Spinner />
   ) : (
     <Fragment>
-      <h1 className="large text-primary">Dashboard</h1>
-      <p className="lead">
-        <i className="fas fa-user"> Welcome {user && user.name}</i>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {user && user.name}
       </p>
-      {profile != null ? (
-        <Fragment>has</Fragment>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions />
+        </Fragment>
       ) : (
         <Fragment>
-          <p>You have not yet setup profile, please add some info</p>
-          <Link to="/create-profile" className="btn btn-primary my-1">
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
             Create Profile
           </Link>
         </Fragment>
@@ -34,16 +37,16 @@ const Dashboard = ({
     </Fragment>
   );
 };
-
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
-
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth,
-  profile: state.profile,
+  profile: state.profile
 });
-
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
