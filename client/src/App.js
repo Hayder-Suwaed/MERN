6d46730ebf58001eab674957f8518d13ede3737b
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-04 02:46:48 -0600

    i have a bug when trying to show my user profile

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import Alert from "./components/layout/Alert";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import { loadUser } from "./actions/auth";
-import setAuthToken from './utils/setAuthToken'
+import setAuthToken from "./utils/setAuthToken";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -26,12 +28,13 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
+          <Route exact path="/" component={Landing} />
+          <section className="container">
             <Alert />
             <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
